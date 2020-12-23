6434b38f50050832176048eb5fd2f468e882b988
Alokit Jindal <iamalokit@gmail.com>
2020-12-23 12:46:04 +0530

    Initial commit spring security

---
+++ spring-security/src/main/java/com/iamalokit/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.iamalokit.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+}
